5a92e923d8aeb003e266bab74e0587618b0bd434
SAM <sangmin990816@gmail.com>
2025-11-04 16:59:12 +0900

    20251104

--- 해결한 문제/b2729_이진수덧셈_ver2.java
+++ 해결한 문제/b2729_이진수덧셈_ver2.java
@@ -3,11 +3,24 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.math.BigInteger;
 
-public class Main {
+public class b2729_이진수덧셈_ver2 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     public static void main(String[] args) throws IOException {
+        int T = Integer.parseInt(br.readLine());
+        for(int t=0; t<T; t++){
+            String[]temp = br.readLine().split(" ");
+
+            BigInteger A = new BigInteger(temp[0], 2);
+            BigInteger B = new BigInteger(temp[1], 2);
+            BigInteger ans = A.add(B);
+
+
+            bw.write(ans.toString(2));
+            bw.newLine();
+        }
 
         bw.flush();
         bw.close();
