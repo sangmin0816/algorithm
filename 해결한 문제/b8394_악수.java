2aad413070573e00fa0c2978e3f3c4a035bbaf84
SAM <sangmin990816@gmail.com>
2025-11-07 22:23:25 +0900

    20251107

--- 해결한 문제/b8394_악수.java
+++ 해결한 문제/b8394_악수.java
@@ -1,20 +1,20 @@
-package 풀다만문제;
 import java.util.*;
 
-public class q8394 { // 피보나치로 푸는 문제라고 한다... 흑흑
+public class b8394_악수 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        double ans = 0;
+        long ans = 0;
 
-        if(n==2) ans = 1;
+        if(n==1) ans = 1;
         else{
             int prev = 1;
             int now = 2;
             for(int i=2; i<n; i++){
-                now=now+prev;
-                prev=now-prev;
+                int temp = now;
+                now = (now+prev)%10;
+                prev = temp;
             }
             ans=now;
         }
